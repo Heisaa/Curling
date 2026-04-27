78d48609b5d5512841a329a4b1825330507a7e71
Heisaa <henriksandhall@gmail.com>
2026-04-27 19:18:28 +0200

    some styling

--- Game.gd
+++ Game.gd
@@ -22,11 +22,21 @@ var sweep_test_started = false
 var end = 1
 var ends_to_play = Global.ends
 var mode = Global.mode
+var current_turn = ""
+var hud_panel
+var hud_panel_style
+var hud_turn_dot_style
+var hud_end_label
+var hud_turn_label
 
 const DESIGN_SIZE = Vector2(1080, 1920)
 const GOAL_Y = 300.0
 const LINE_OVER_BOTTOM_MARGIN = 420.0
 const SCORE_BOTTOM_MARGIN = 350.0
+const HUD_WIDTH = 620.0
+const HUD_BOTTOM_MARGIN = 18.0
+const HUD_BORDER_WIDTH = 14
+const HUD_CORNER_RADIUS = 36
 const SWEEP_TEST_STONE_SPEED = 940.0
 const FontSizes = preload("res://UI/FontSizes.gd")
 const ScoreCalculator = preload("res://ScoreCalculator.gd")
@@ -48,8 +58,10 @@ func _ready():
 	$Rink.body_exited.connect(_on_rink_body_exited)
 	_apply_font_sizes()
 	init_score()
+	_create_hud()
 	get_viewport().size_changed.connect(_layout_for_viewport)
 	_layout_for_viewport()
+	_update_hud()
 	if Global.sweep_test:
 		_start_sweep_test()
 
@@ -104,6 +116,7 @@ func _process(_delta):
 			sweeping_controller.reset_ice()
 			red_played = 0
 			yellow_played = 0
+			current_turn = ""
 			
 			if end % 2 == 0:
 				# end is even
@@ -111,7 +124,9 @@ func _process(_delta):
 			else:
 				# end is odd
 				last_played = "yellow"
-		
+
+		_update_hud()
+			
 	else:
 		play_round()
 
@@ -205,6 +220,8 @@ func create_red_stone():
 		red_stones.append(stone)
 		red_played += 1
 		last_played = "red"
+		current_turn = "Red"
+		_update_hud()
 
 func create_yellow_stone():
 	if yellow_played < max_stones:
@@ -215,12 +232,16 @@ func create_yellow_stone():
 		yellow_stones.append(stone)
 		yellow_played += 1
 		last_played = "yellow"
+		current_turn = "Yellow"
+		_update_hud()
 
 func _start_sweep_test():
 	if sweep_test_started:
 		return
 
 	sweep_test_started = true
+	if hud_panel != null:
+		hud_panel.visible = false
 	$RedScore.visible = false
 	$YellowScore.visible = false
 	for sprite in red_score_sprites + yellow_score_sprites:
@@ -279,6 +300,95 @@ func init_score():
 func _apply_font_sizes():
 	$RedScore.add_theme_font_size_override("font_size", FontSizes.SCORE)
 	$YellowScore.add_theme_font_size_override("font_size", FontSizes.SCORE)
+	if hud_end_label != null:
+		hud_end_label.add_theme_font_size_override("font_size", 30)
+		hud_turn_label.add_theme_font_size_override("font_size", 34)
+
+func _create_hud():
+	var hud_layer = CanvasLayer.new()
+	hud_layer.name = "HUD"
+	add_child(hud_layer)
+
+	hud_panel = PanelContainer.new()
+	hud_panel.name = "MatchStatus"
+	hud_layer.add_child(hud_panel)
+
+	hud_panel_style = StyleBoxFlat.new()
+	hud_panel_style.bg_color = Color(0.05, 0.06, 0.06, 0.86)
+	hud_panel_style.border_color = Color(1.0, 1.0, 1.0, 0.20)
+	hud_panel_style.border_width_left = HUD_BORDER_WIDTH
+	hud_panel_style.border_width_top = HUD_BORDER_WIDTH
+	hud_panel_style.border_width_right = HUD_BORDER_WIDTH
+	hud_panel_style.border_width_bottom = HUD_BORDER_WIDTH
+	hud_panel_style.corner_radius_top_left = HUD_CORNER_RADIUS
+	hud_panel_style.corner_radius_top_right = HUD_CORNER_RADIUS
+	hud_panel_style.corner_radius_bottom_left = HUD_CORNER_RADIUS
+	hud_panel_style.corner_radius_bottom_right = HUD_CORNER_RADIUS
+	hud_panel.add_theme_stylebox_override("panel", hud_panel_style)
+
+	var margin = MarginContainer.new()
+	margin.add_theme_constant_override("margin_left", 22)
+	margin.add_theme_constant_override("margin_top", 8)
+	margin.add_theme_constant_override("margin_right", 22)
+	margin.add_theme_constant_override("margin_bottom", 8)
+	hud_panel.add_child(margin)
+
+	var layout = HBoxContainer.new()
+	layout.alignment = BoxContainer.ALIGNMENT_CENTER
+	layout.add_theme_constant_override("separation", 18)
+	margin.add_child(layout)
+
+	var hud_turn_dot = Panel.new()
+	hud_turn_dot.custom_minimum_size = Vector2(18.0, 18.0)
+	hud_turn_dot.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
+	hud_turn_dot.size_flags_vertical = Control.SIZE_SHRINK_CENTER
+	hud_turn_dot_style = StyleBoxFlat.new()
+	hud_turn_dot_style.bg_color = Color.WHITE
+	hud_turn_dot_style.corner_radius_top_left = 9
+	hud_turn_dot_style.corner_radius_top_right = 9
+	hud_turn_dot_style.corner_radius_bottom_left = 9
+	hud_turn_dot_style.corner_radius_bottom_right = 9
+	hud_turn_dot.add_theme_stylebox_override("panel", hud_turn_dot_style)
+	layout.add_child(hud_turn_dot)
+
+	hud_turn_label = Label.new()
+	hud_turn_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	hud_turn_label.add_theme_color_override("font_color", Color.WHITE)
+	layout.add_child(hud_turn_label)
+
+	hud_end_label = Label.new()
+	hud_end_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	hud_end_label.add_theme_color_override("font_color", Color(0.84, 0.88, 0.90, 1.0))
+	layout.add_child(hud_end_label)
+
+	_apply_font_sizes()
+
+func _update_hud():
+	if hud_panel == null:
+		return
+
+	var turn_name = current_turn
+	if game_over:
+		turn_name = ""
+		hud_turn_label.text = "Game Over"
+	elif current_turn == "":
+		turn_name = _get_next_turn_name()
+		hud_turn_label.text = turn_name + " to throw"
+	else:
+		hud_turn_label.text = turn_name + " to throw"
+
+	var turn_color = _get_turn_color(turn_name)
+	hud_turn_dot_style.bg_color = turn_color
+	hud_panel_style.border_color = turn_color
+
+	hud_end_label.text = "End " + str(min(end, ends_to_play)) + "/" + str(ends_to_play)
+
+func _get_turn_color(turn_name):
+	if turn_name == "Red":
+		return Color(1.0, 0.12, 0.10, 1.0)
+	if turn_name == "Yellow":
+		return Color(1.0, 0.88, 0.08, 1.0)
+	return Color(1.0, 1.0, 1.0, 0.25)
 
 func _layout_for_viewport():
 	var viewport_size = Vector2(get_viewport().get_visible_rect().size)
@@ -304,11 +414,41 @@ func _layout_for_viewport():
 
 	$RedScore.position = Vector2(5, score_y + 70)
 	$YellowScore.position = Vector2(viewport_size.x - 105, score_y + 70)
+	if hud_panel != null:
+		var hud_width = min(viewport_size.x - 40.0, HUD_WIDTH)
+		hud_panel.size = Vector2(hud_width, 0.0)
+		var hud_height = hud_panel.get_combined_minimum_size().y
+		hud_panel.position = Vector2(center_x - hud_width / 2.0, viewport_size.y - hud_height - HUD_BOTTOM_MARGIN)
 
 	for i in range(red_score_sprites.size()):
 		red_score_sprites[i].position = Vector2((i * 50) + 30, score_y)
 	for i in range(yellow_score_sprites.size()):
 		yellow_score_sprites[i].position = Vector2(viewport_size.x - 30 - (i * 50), score_y)
+
+func _get_next_turn_name():
+	if mode == "Curling":
+		if last_played == "red":
+			return "Yellow"
+		return "Red"
+
+	var closest_red = _get_closest_active_stone(red_stones)
+	var closest_yellow = _get_closest_active_stone(yellow_stones)
+	if red_played >= max_stones and yellow_played >= max_stones:
+		return "No one"
+	if (closest_red < closest_yellow and yellow_played < max_stones) or red_played >= max_stones:
+		return "Yellow"
+	if (closest_red > closest_yellow and red_played < max_stones) or yellow_played >= max_stones:
+		return "Red"
+	if last_played == "yellow":
+		return "Red"
+	return "Yellow"
+
+func _get_closest_active_stone(stones):
+	var closest = 2000.0
+	for stone in stones:
+		if is_instance_valid(stone) and stone.is_out == false:
+			closest = min(stone.global_position.distance_to($Goal.global_position), closest)
+	return closest
 
 func update_score():
 	for i in max_stones:
